74a57659d7921486f1be796b35aa5ac642cb9954
Anuradha Dissanayake <falconne@gmail.com>
2017-05-27 20:03:02 +1200

    Add placeholder About.xml

--- Startup.cs
+++ Startup.cs
@@ -5,7 +5,8 @@ using System.Text;
 
 namespace ImprovedWorkshops
 {
-    public class Startup
+    public class Startup : HugsLib.ModBase
     {
+        public override string ModIdentifier => "ImprovedWorkshops";
     }
 }
