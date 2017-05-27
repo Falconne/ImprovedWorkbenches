cd679a8f8c2305a03a4108302d8fcbcafa353e07
Anuradha Dissanayake <falconne@gmail.com>
2017-05-28 00:33:21 +1200

    Test code

--- BillConfig_DoWindowContents_Patch.cs
+++ BillConfig_DoWindowContents_Patch.cs
@@ -1,4 +1,5 @@
-﻿using Harmony;
+﻿using System.Linq;
+using Harmony;
 using RimWorld;
 using UnityEngine;
 using Verse;
@@ -12,14 +13,41 @@ namespace ImprovedWorkbenches
         public static void DrawHelloButton(Dialog_BillConfig __instance, Rect inRect)
         {
             var bill = (Bill_Production)AccessTools.Field(typeof(Dialog_BillConfig), "bill").GetValue(__instance);
-            if (bill.repeatMode != BillRepeatModeDefOf.RepeatCount)
+            if (bill.repeatMode != BillRepeatModeDefOf.TargetCount)
                 return;
 
-            var buttonSize = new Vector2(120f, 40f);
-            if (Widgets.ButtonText(new Rect(0, inRect.height - buttonSize.y, buttonSize.x, buttonSize.y), "FooBar"))
+            if (!(bill.recipe.products != null &&
+                  bill.recipe.products.Count > 0 &&
+                  bill.recipe.products.First().thingDef.BaseMarketValue > 0))
             {
-                // do stuff
+                return;
             }
+
+            var thingDef = bill.recipe.products.First().thingDef;
+            var thingFilter = new ThingFilter();
+            thingFilter.SetDisallowAll();
+            thingFilter.SetAllow(thingDef, true);
+
+            var rect = new Rect(0, inRect.height - 200f, 160f, 40f);
+            Widgets.Label(rect, "Counted items filter:");
+            var row = inRect.height - 150f;
+            DrawHitPointsFilterConfig(ref row, rect.width, thingFilter);
+
+        }
+
+        private static void DrawHitPointsFilterConfig(ref float y, float width, ThingFilter filter)
+        {
+            if (!filter.allowedHitPointsConfigurable)
+            {
+                return;
+            }
+            Rect rect = new Rect(0f, y, width, 26f);
+            FloatRange allowedHitPointsPercents = filter.AllowedHitPointsPercents;
+            Widgets.FloatRange(rect, 1, ref allowedHitPointsPercents, 0f, 1f, "HitPoints", ToStringStyle.PercentZero);
+            filter.AllowedHitPointsPercents = allowedHitPointsPercents;
+            y += 26f;
+            y += 5f;
+            Text.Font = GameFont.Small;
         }
     }
 }
