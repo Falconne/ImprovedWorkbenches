58eee71c776c8f678bad7b9f79447b7eaf208df9
Anuradha Dissanayake <falconne@gmail.com>
2017-05-27 23:16:22 +1200

    Add helper post build step

--- BillConfig_DoWindowContents_Patch.cs
+++ BillConfig_DoWindowContents_Patch.cs
@@ -11,6 +11,10 @@ namespace ImprovedWorkbenches
         [HarmonyPostfix]
         public static void DrawHelloButton(Dialog_BillConfig __instance, Rect inRect)
         {
+            var bill = (Bill_Production)AccessTools.Field(typeof(Dialog_BillConfig), "bill").GetValue(__instance);
+            if (bill.repeatMode != BillRepeatModeDefOf.RepeatCount)
+                return;
+
             var buttonSize = new Vector2(120f, 40f);
             if (Widgets.ButtonText(new Rect(0, inRect.height - buttonSize.y, buttonSize.x, buttonSize.y), "FooBar"))
             {
