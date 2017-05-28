f0da69132bfee92bb64bb4f71ecfe7745e653a51
Anuradha Dissanayake <falconne@gmail.com>
2017-05-28 18:16:23 +1200

    Detours with CCL

---
+++ BillUtility_Detour.cs
@@ -0,0 +1,19 @@
+﻿using Harmony;
+using RimWorld;
+using Verse;
+
+namespace ImprovedWorkbenches
+{
+    public static class BillUtility_Detour
+    {
+        public static Bill MakeNewBill(this RecipeDef recipe)
+        {
+            if (recipe.UsesUnfinishedThing)
+            {
+                return new Bill_ProductionWithUftWithFilters(recipe);
+            }
+
+            return new Bill_ProductionWithFilters(recipe);
+        }
+    }
+}
---
+++ Bill_ProductionWithFilters.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using Verse;
+
+namespace ImprovedWorkbenches
+{
+    public class Bill_ProductionWithFilters : Bill_Production
+    {
+        public Bill_ProductionWithFilters(RecipeDef recipe) : base(recipe)
+        {
+        }
+
+        public ThingFilter OutputFilter = new ThingFilter();
+
+        public bool UseOutputFilter = false;
+    }
+
+    public class Bill_ProductionWithUftWithFilters : Bill_ProductionWithUft
+    {
+        public Bill_ProductionWithUftWithFilters(RecipeDef recipe) : base(recipe)
+        {
+        }
+
+        public ThingFilter OutputFilter = new ThingFilter();
+
+        public bool UseOutputFilter = false;
+    }
+}
