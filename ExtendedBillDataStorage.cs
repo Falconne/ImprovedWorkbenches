7518afd50d2080105ecfea76448142c380aae883
Anuradha Dissanayake <falconne@gmail.com>
2017-06-17 21:05:26 +1200

    Refactor mod storage WIP

--- ExtendedBillDataStorage.cs
+++ ExtendedBillDataStorage.cs
@@ -1,9 +1,25 @@
 ﻿using System.Collections.Generic;
+using ImprovedWorkbenches;
+using RimWorld;
+using Verse;
 
 namespace ImprovedWorkbenches
 {
-    public class ExtendedBillDataStorage
+    public class ExtendedBillDataStorage : IExposable
     {
-        private Dictionary<string, ExtendedBillData> store = new Dictionary<string, ExtendedBillData>();
+        private Dictionary<int, ExtendedBillData> store =
+            new Dictionary<int, ExtendedBillData>();
+
+        private List<int> _billIDsWorkingList;
+
+        private List<ExtendedBillData> _extendedBillDataWorkingList;
+
+        public void ExposeData()
+        {
+			Scribe_Collections.Look(
+                ref store, "store", 
+                LookMode.Value, LookMode.Deep, 
+                ref _billIDsWorkingList, ref _extendedBillDataWorkingList);
+        }
     }
 }
