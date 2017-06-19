99e32f7c47d2123f76cd771435cc6ed843d849f6
Anuradha Dissanayake <falconne@gmail.com>
2017-06-19 16:31:11 +1200

    Add workaround for Bill repeat mode button interfering with link updates.
    The GUI is not event driven, so we can't hook into change events to propagate updates.

---
+++ Detours/BillRepeatModeUtility_MakeConfigFloatMenu_Detour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Harmony;
+using RimWorld;
+using Verse;
+
+namespace ImprovedWorkbenches
+{
+    [HarmonyPatch(typeof(BillRepeatModeUtility), "MakeConfigFloatMenu")]
+    public class BillRepeatModeUtility_MakeConfigFloatMenu_Detour
+    {
+        [HarmonyPrefix]
+        static bool Prefix(Bill_Production bill)
+        {
+            if (!Main.Instance.GetExtendedBillDataStorage().IsLinkedBill(bill))
+                return true;
+
+            var list = new List<FloatMenuOption>();
+            list.Add(new FloatMenuOption("Use Details to change linked bill mode", delegate {}));
+            Find.WindowStack.Add(new FloatMenu(list));
+
+            return false;
+        }
+    }
+}
