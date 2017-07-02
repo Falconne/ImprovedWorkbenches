09f967fa38a5147636a8b8c66555d5075312486d
Anuradha Dissanayake <falconne@gmail.com>
2017-07-02 19:03:11 +1200

    Show resume level in bill stack overview

---
+++ Detours/Bill_Production_RepeatInfoText_Detour.cs
@@ -0,0 +1,25 @@
+﻿using Harmony;
+using RimWorld;
+
+namespace ImprovedWorkbenches
+{
+    [HarmonyPatch(typeof(Bill_Production), "get_RepeatInfoText")]
+    public class Bill_Production_RepeatInfoText_Detour
+    {
+        [HarmonyPrefix]
+        static bool Prefix(Bill_Production __instance, ref string __result)
+        {
+            if (__instance.repeatMode != BillRepeatModeDefOf.TargetCount ||
+                !__instance.pauseWhenSatisfied)
+            {
+                return true;
+            }
+
+            var currentCount = __instance.recipe.WorkerCounter.CountProducts(__instance);
+
+            __result = $"({__instance.unpauseWhenYouHave}) {currentCount}/{__instance.targetCount}";
+
+            return false;
+        }
+    }
+}
