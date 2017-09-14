3d40090c0b7bdeefe9151a014b66f1cf617e9129
Anuradha Dissanayake <falconne@gmail.com>
2017-09-14 23:31:32 +1200

    Add button to workbench overview screen to switch job storage between "Drop on Floor" and "Take to Best Stockpile"

--- Detours/Bill_Production_DoConfigInterface_Detour.cs
+++ Detours/Bill_Production_DoConfigInterface_Detour.cs
@@ -16,7 +16,21 @@ namespace ImprovedWorkbenches
         [HarmonyPostfix]
         public static void Postfix(Bill_Production __instance, Rect baseRect, Color baseColor)
         {
-            Rect copyBillRect = new Rect(baseRect.xMax - 80f, baseRect.y, 24f, 24f);
+            var storeModeImage = Resources.BestStockpile;
+            var nextStoreMode = BillStoreModeDefOf.DropOnFloor;
+            if (__instance.storeMode == BillStoreModeDefOf.DropOnFloor)
+            {
+                storeModeImage = Resources.DropOnFloor;
+                nextStoreMode = BillStoreModeDefOf.BestStockpile;
+            }
+
+            var storeModeRect = new Rect(baseRect.xMax - 80f, baseRect.y, 24f, 24f);
+            if (Widgets.ButtonImage(storeModeRect, storeModeImage, baseColor))
+            {
+                __instance.storeMode = nextStoreMode;
+            }
+
+            var copyBillRect = new Rect(storeModeRect.xMin - 28f, baseRect.y, 24f, 24f);
             if (Widgets.ButtonImage(copyBillRect, Resources.CopyButton, baseColor))
             {
                 Main.Instance.BillCopyPasteHandler.DoCopy(__instance);
@@ -27,13 +41,12 @@ namespace ImprovedWorkbenches
             if (!extendedBillDataStorage.IsLinkedBill(__instance))
                 return;
 
-            Rect breakLinkRect = new Rect(copyBillRect.xMin - 28f, baseRect.y, 24f, 24f);
+            var breakLinkRect = new Rect(copyBillRect.xMin - 28f, baseRect.y, 24f, 24f);
             if (Widgets.ButtonImage(breakLinkRect, Resources.BreakLink, baseColor))
             {
                 extendedBillDataStorage.RemoveBillFromLinkSets(__instance);
             }
             TooltipHandler.TipRegion(breakLinkRect, "Break link to other bills");
-
         }
     }
 }
--- Resources.cs
+++ Resources.cs
@@ -6,14 +6,18 @@ namespace ImprovedWorkbenches
     [StaticConstructorOnStartup]
     public class Resources
     {
-        public static Texture2D CopyButton = ContentFinder<Texture2D>.Get("UI/Buttons/Copy");
+        public static readonly Texture2D CopyButton = ContentFinder<Texture2D>.Get("UI/Buttons/Copy");
 
-        public static Texture2D BreakLink = ContentFinder<Texture2D>.Get("BreakLink");
+        public static readonly Texture2D BreakLink = ContentFinder<Texture2D>.Get("BreakLink");
 
-        public static Texture2D LeftArrow = ContentFinder<Texture2D>.Get("LeftArrow");
+        public static readonly Texture2D LeftArrow = ContentFinder<Texture2D>.Get("LeftArrow");
 
-        public static Texture2D RightArrow = ContentFinder<Texture2D>.Get("RightArrow");
+        public static readonly Texture2D RightArrow = ContentFinder<Texture2D>.Get("RightArrow");
 
         public static readonly Texture2D Rename = ContentFinder<Texture2D>.Get("UI/Buttons/Rename");
+
+        public static readonly Texture2D DropOnFloor = ContentFinder<Texture2D>.Get("DropOnFloor");
+
+        public static readonly Texture2D BestStockpile = ContentFinder<Texture2D>.Get("BestStockpile");
     }
 }
