5ace9494f9955e2e5e11ea3b37a5137c33704d40
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-03-19 03:50:37 -0700

    Dialog_MessageBox was messing with mouse scrolling so use Window instead and shuffle around button/title declarations

--- src/ImprovedWorkbenches/Custom Output Filter/Dialog_OutputFilter.cs
+++ src/ImprovedWorkbenches/Custom Output Filter/Dialog_OutputFilter.cs
@@ -8,34 +8,23 @@ using UnityEngine;
 namespace ImprovedWorkbenches
 {
     [StaticConstructorOnStartup]
-    public class Dialog_ThingFilter : Dialog_MessageBox
+    public class Dialog_ThingFilter : Window
     {
         public ThingFilter filter;
+        public ExtendedBillData extendedBill;
 
         public Vector2 scrollPosition;
 
         public Window reOpenWindow;
 
-        public Dialog_ThingFilter(ExtendedBillData extendedBill, Window w) : base("Do Until X also includes these:")
+        public Dialog_ThingFilter(ExtendedBillData e, Window w)
         {
             reOpenWindow = w;
+            extendedBill = e;
+
             filter = new ThingFilter();
             if (extendedBill.ProductAdditionalFilter != null)
                 filter.CopyAllowancesFrom(extendedBill.ProductAdditionalFilter);
-
-            buttonAAction = () =>
-            {
-                if (extendedBill.ProductAdditionalFilter == null)
-                    extendedBill.ProductAdditionalFilter = filter;
-                else
-                    extendedBill.ProductAdditionalFilter.CopyAllowancesFrom(filter);
-            };
-
-            buttonBText = "Stop Using Filter";
-            buttonBAction = () =>
-            {
-                extendedBill.ProductAdditionalFilter = null;
-            };
         }
 
         //to hide HP, quality, special filters, this is needed:
@@ -51,14 +40,34 @@ namespace ImprovedWorkbenches
         static List<SpecialThingFilterDef> specialThingDefs = DefDatabase<SpecialThingFilterDef>.AllDefs.ToList();
         public override void DoWindowContents(Rect inRect)
         {
-            base.DoWindowContents(inRect);
+            Rect titleRect = inRect.TopPartPixels(40);
+            Rect buttonsRect = inRect.BottomPartPixels(40);
+            Rect cancelRect = buttonsRect.LeftHalf();
+            Rect okayRect = buttonsRect.RightHalf();
+            Rect filterRect = inRect;
+            filterRect.height -= 80;
+            filterRect.y += 35;
 
-            Rect filterRect = inRect.BottomPartPixels(inRect.height - 40);
-            filterRect.height -= 40;
+            Widgets.Label(titleRect, "Do Until X also includes these:");
+
             ThingFilterUI.DoThingFilterConfigWindow(filterRect, ref scrollPosition, filter,
                 openMask: TreeOpenMasks.ThingFilter,
                 forceHiddenFilters: specialThingDefs,
                 parentFilter: baseFilter);
+
+            if (Widgets.ButtonText(cancelRect, "Stop Using Filter"))
+            {
+                extendedBill.ProductAdditionalFilter = null;
+                Close();
+            }
+            if (Widgets.ButtonText(okayRect, "OK"))
+            {
+                if (extendedBill.ProductAdditionalFilter == null)
+                    extendedBill.ProductAdditionalFilter = filter;
+                else
+                    extendedBill.ProductAdditionalFilter.CopyAllowancesFrom(filter);
+                Close();
+            }
         }
 
         public override void PreClose()
