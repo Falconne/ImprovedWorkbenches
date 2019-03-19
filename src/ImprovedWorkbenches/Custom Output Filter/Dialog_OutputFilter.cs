c08610e513d58d3ef907fbc5768d7ad33484f3bc
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-03-19 02:06:18 -0700

    Don't need to keep _extendedBill, remove log messages

--- src/ImprovedWorkbenches/Custom Output Filter/Dialog_OutputFilter.cs
+++ src/ImprovedWorkbenches/Custom Output Filter/Dialog_OutputFilter.cs
@@ -9,34 +9,30 @@ namespace ImprovedWorkbenches
 {
     public class Dialog_ThingFilter : Dialog_MessageBox
     {
-        private ExtendedBillData _extendedBill;
-
         public ThingFilter filter;
 
         public Vector2 scrollPosition;
 
         public Dialog_ThingFilter(ExtendedBillData extendedBill) : base("Product Filter")
         {
-            _extendedBill = extendedBill;
-
             filter = new ThingFilter();
-            if (_extendedBill.ProductAdditionalFilter != null)
-                filter.CopyAllowancesFrom(_extendedBill.ProductAdditionalFilter);
+            if (extendedBill.ProductAdditionalFilter != null)
+                filter.CopyAllowancesFrom(extendedBill.ProductAdditionalFilter);
 
             buttonAAction = () =>
             {
                 Log.Message("APPLIED");
-                if (_extendedBill.ProductAdditionalFilter == null)
-                    _extendedBill.ProductAdditionalFilter = filter;
+                if (extendedBill.ProductAdditionalFilter == null)
+                    extendedBill.ProductAdditionalFilter = filter;
                 else
-                    _extendedBill.ProductAdditionalFilter.CopyAllowancesFrom(filter);
+                    extendedBill.ProductAdditionalFilter.CopyAllowancesFrom(filter);
             };
 
             buttonBText = "Default Filter";
             buttonBAction = () =>
             {
                 Log.Message("Clear");
-                _extendedBill.ProductAdditionalFilter = null;
+                extendedBill.ProductAdditionalFilter = null;
             };
         }
 
