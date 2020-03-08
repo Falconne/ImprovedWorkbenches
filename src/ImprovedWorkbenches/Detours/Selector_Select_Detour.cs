7f02f4b1d39d7d92e765c0d267dc2b8100e2b300
Anuradha Dissanayake <falconne@gmail.com>
2020-03-09 09:16:01 +1300

    Some refactoring

--- src/ImprovedWorkbenches/Detours/Selector_Select_Detour.cs
+++ src/ImprovedWorkbenches/Detours/Selector_Select_Detour.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 using RimWorld;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Verse;
 
@@ -12,83 +13,78 @@ namespace ImprovedWorkbenches
     {
         public static void Postfix(Selector __instance)
         {
-            if (!Main.Instance.ShouldExpandBillsTab()) 
+            if (!Main.Instance.ShouldExpandBillsTab())
                 return;
 
-            // Has one single thing been selected:
-            if (__instance.NumSelected != 1) 
-                return;
-
-            Zone z = __instance.SelectedZone;
-            Thing t = __instance.SingleSelectedThing;
-            // Do we have either a stockpile or a worktable or a storage building?
-            // If not, don't try to open the wrong tab:
-            if ((z != null && !(z is Zone_Stockpile)) || // is it a non Stockpile zone?
-                (z == null && (t == null // not a zone nor a building
-                                         // it's a thing, but not a building we care about
-                               || !(t is Building_WorkTable || t is Building_Storage)
-                               )
-                 )) return;
-
-            // I'm writing this, so I get to build in compatibility for myself!
-            if ((t as Building_Storage != null) && ModLister.HasActiveModWithName("LWM's Deep Storage"))
+            try
             {
-                return;  // Deep Storage handles tabs on its own for storage buildings
-            }
+                // Has one single thing been selected:
+                if (__instance.NumSelected != 1)
+                    return;
 
-            // Fill tab with the proper tab we want open for the new selection
-            ITab tab = null;
-            // But check to see if a tab is already open - we don't want
-            //   to override an open tab if the same tab can be open for
-            //   the new selection
-            var pane = (MainTabWindow_Inspect)MainButtonDefOf.Inspect.TabWindow;
-            Type alreadyOpenTabType = pane.OpenTabType;
-            if (alreadyOpenTabType != null)
-            {
-                // this is the list of all the tabs the newly selected thing CAN have
-                System.Collections.Generic.IEnumerable<InspectTabBase> listOfTabs;
-                if (z != null)
-                    listOfTabs = z.GetInspectTabs();
-                else
-                    listOfTabs = t.GetInspectTabs();
+                var tabsOfSelectedThing = GetTabsOfAnySelectedThingWeCareAbout(__instance)?.ToList();
+                if (tabsOfSelectedThing == null)
+                    return;
 
-                foreach (var x in listOfTabs)
+                // If any tab of selected thing is currently open, don't override it
+                var pane = (MainTabWindow_Inspect) MainButtonDefOf.Inspect.TabWindow;
+                Type alreadyOpenTabType = pane.OpenTabType;
+                if (alreadyOpenTabType != null)
                 {
-                    // This misses any subclassing, but that's probably okay?
-                    // If it's not, add in some .isSubClassOf()s
-                    if (x.GetType() == alreadyOpenTabType)
+                    foreach (var tab in tabsOfSelectedThing)
                     {
-                        return; // standard Selector behavior should kick in.
+                        // This misses any subclassing, but that's probably okay?
+                        // If it's not, add in some .isSubClassOf()s
+                        if (tab.GetType() == alreadyOpenTabType)
+                        {
+                            // Leave selected tab alone
+                            return;
+                        }
                     }
-                    // If it has Storage or Bills tab, go ahead and select it!
-                    if (x.GetType() == typeof(ITab_Storage)) tab = (ITab)x;
-                    if (x.GetType() == typeof(ITab_Bills)) tab = (ITab)x;
                 }
+
+                // Select any storage or bill tab the selected thing might have
+                //var tabToSelect = listOfTabs.OfType<ITab_Storage>().FirstOrDefault();
+                var tabToSelect = tabsOfSelectedThing
+                    .FirstOrDefault(t => t is ITab_Storage || t is ITab_Bills);
+
+                if (tabToSelect == null)
+                    return;
+
+                // These two things are what makes a tab open:
+                tabToSelect.OnOpen();
+                pane.OpenTabType = tabToSelect.GetType();
             }
-            else
-            { // We still need to grab the Storage or Bills tab:
-                // If it's a zone, open Storage!
-                if (z != null)
-                {
-                    tab = z.GetInspectTabs().OfType<ITab_Storage>().First();
-                }
-                else if (t is Building_Storage)
-                { // same for storage buildings
-                    tab = t.GetInspectTabs().OfType<ITab_Storage>().First();
-                }
-                else
-                { // workbench: open Bills!
-                    tab = t.GetInspectTabs().OfType<ITab_Bills>().First();
-                }
-            }
-            if (tab == null)
+            catch (Exception e)
             {
-                Log.Warning("" + (z != null ? ("Zone " + z) : ("Building " + t)) + " does not have a Storage or Bills tab?");
-                return;
+                Main.Instance.Logger.Error(
+                    "Error trying to select bill or storage tab. Please report this to the Better Workbench Management mod page");
+                Main.Instance.Logger.Error(e.Message);
+                Main.Instance.Logger.Error(e.StackTrace);
             }
-            // These two things are what makes a tab open:
-            tab.OnOpen();
-            pane.OpenTabType = tab.GetType();
         }
-    } // end patch of Select to open ITab
+
+        private static IEnumerable<InspectTabBase> GetTabsOfAnySelectedThingWeCareAbout(Selector __instance)
+        {
+            if (__instance.SelectedZone is Zone_Stockpile stockpile)
+                return stockpile.GetInspectTabs();
+
+            if (__instance.SingleSelectedThing is Building_WorkTable workTable)
+                return workTable.GetInspectTabs();
+
+            if (__instance.SingleSelectedThing is Building_Storage storage)
+            {
+                if (ModLister.HasActiveModWithName("LWM's Deep Storage"))
+                {
+                    // Deep Storage handles tabs on its own for storage buildings
+                    return null;
+                }
+
+                return storage.GetInspectTabs();
+            }
+
+            return null;
+        }
+    }
+
 }
