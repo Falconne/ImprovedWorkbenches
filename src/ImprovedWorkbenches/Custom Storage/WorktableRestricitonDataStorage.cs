6589497cde0f41376c5331d6f304f45e72ef74f4
AllenCaine <56565659+AllenCaine@users.noreply.github.com>
2025-07-11 14:56:30 +0200

    Refactor worktable restriction handling by removing obsolete XML definitions and enhancing bill mirroring logic. Updated WorktableRestrictionData to support bill restrictions and improved error handling in detours.

--- src/ImprovedWorkbenches/Custom Storage/WorktableRestricitonDataStorage.cs
+++ src/ImprovedWorkbenches/Custom Storage/WorktableRestricitonDataStorage.cs
@@ -1,6 +1,8 @@
 using Verse;
 using RimWorld.Planet;
 using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
 
 namespace ImprovedWorkbenches
 {
@@ -26,12 +28,13 @@ namespace ImprovedWorkbenches
             }
             Scribe_Collections.Look(ref _worktableRestrictionDictonary, "worktableRestrictionDictonary", LookMode.Value, LookMode.Deep, ref workTableIds, ref componentData);
         }
-
+        #region XML Comment
         /// <summary>
         /// Retrieves the worktable restriction data for a specific worktable.
         /// </summary>
         /// <param name="workTableId">The unique identifier of the worktable.</param>
         /// <returns>The worktable restriction data if found; otherwise, a new WorktableRestrictionData is created and returned.</returns>
+        #endregion
         public WorktableRestrictionData GetWorktableRestrictionData(int workTableId)
         {
             if (_worktableRestrictionDictonary.TryGetValue(workTableId, out WorktableRestrictionData data))
@@ -41,12 +44,28 @@ namespace ImprovedWorkbenches
             SetWorktableRestrictionData(workTableId, new WorktableRestrictionData());
             return GetWorktableRestrictionData(workTableId);
         }
+        #region XML Comment
+        /// <summary>
+        /// Retrieves the worktable restriction data for a specific bill.
+        /// </summary>
+        /// <param name="bill">The bill to get the worktable restriction data for.</param>
+        /// <returns>The worktable restriction data if found; otherwise, null.</returns>
+        #endregion
+        public WorktableRestrictionData GetWorktableRestrictionData(Bill_Production bill)
+        {
+            if (bill?.billStack?.billGiver is Building_WorkTable workTable)
+                return GetWorktableRestrictionData(workTable.thingIDNumber);
 
+            return null;
+        }
+
+        #region XML Comment
         /// <summary>
         /// Adds or updates the worktable restriction data for a specific worktable.
         /// </summary>
         /// <param name="workTableId">The unique identifier of the worktable.</param>
         /// <param name="data">The worktable restriction data to store.</param>
+        #endregion
         public void SetWorktableRestrictionData(int workTableId, WorktableRestrictionData data)
         {
             if (_worktableRestrictionDictonary.ContainsKey(workTableId))
@@ -55,10 +74,12 @@ namespace ImprovedWorkbenches
                 _worktableRestrictionDictonary.Add(workTableId, data);
         }
 
+        #region XML Comment
         /// <summary>
         /// Removes the worktable restriction data for a specific worktable.
         /// </summary>
         /// <param name="workTableId">The unique identifier of the worktable.</param>
+        #endregion
         public void RemoveWorktableRestrictionData(int workTableId)
         {
             if (_worktableRestrictionDictonary.ContainsKey(workTableId))
--- src/ImprovedWorkbenches/Custom Storage/WorktableRestrictionData.cs
+++ src/ImprovedWorkbenches/Custom Storage/WorktableRestrictionData.cs
@@ -9,7 +9,7 @@ using System;
 namespace ImprovedWorkbenches
 {
     // Component that saves worktable restrictions for each worktable
-    public class WorktableRestrictionData
+    public class WorktableRestrictionData : IExposable
     {
         public bool isRestricted = false;
         public Pawn restrictionPawn = null;
@@ -20,6 +20,43 @@ namespace ImprovedWorkbenches
 
         public WorktableRestrictionData()
         {
+        }
+
+        public void ExposeData()
+        {
+            Scribe_Values.Look(ref isRestricted, "isRestricted", false);
+            Scribe_References.Look(ref restrictionPawn, "restrictionPawn");
+            Scribe_Values.Look(ref restrictionSlavesOnly, "restrictionSlavesOnly", false);
+            Scribe_Values.Look(ref restrictionMechsOnly, "restrictionMechsOnly", false);
+            Scribe_Values.Look(ref restrictionNonMechsOnly, "restrictionNonMechsOnly", false);
+            Scribe_Values.Look(ref restrictionAllowedSkillRange, "restrictionAllowedSkillRange", new IntRange(0, 20));
+        }
+
+        public void SetWorktableRestrictionToBill(Bill_Production bill)
+        {
+            if (restrictionPawn != null)
+            {
+                bill.SetPawnRestriction(restrictionPawn);
+            }
+            else if (restrictionSlavesOnly)
+            {
+                bill.SetAnySlaveRestriction();
+                bill.allowedSkillRange = restrictionAllowedSkillRange;
+            }
+            else if (restrictionMechsOnly)
+            {
+                bill.SetAnyMechRestriction();
+            }
+            else if (restrictionNonMechsOnly)
+            {
+                bill.SetAnyNonMechRestriction();
+                bill.allowedSkillRange = restrictionAllowedSkillRange;
+            }
+            else
+            {
+                bill.SetAnyPawnRestriction();
+                bill.allowedSkillRange = restrictionAllowedSkillRange;
+            }
         }
     }
 }
